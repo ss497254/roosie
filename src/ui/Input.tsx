3d4bf25ce3c7445e6e0308a41903e35c40e01ce4
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-15 18:47:52 +0530

    feat: Input and Loading ui component

---
+++ src/ui/Input.tsx
@@ -0,0 +1,79 @@
+import React, { HTMLProps, forwardRef, useId, useState } from "react";
+import { IconButton } from "../components/button";
+
+import EyeOffIcon from "src/icons/eye-off.svg";
+import EyeIcon from "src/icons/eye.svg";
+
+export type InputProps = React.HTMLProps<HTMLTextAreaElement> & {
+  autoHeight?: boolean;
+  rows?: number;
+};
+
+export function Input(props: InputProps) {
+  return (
+    <textarea {...props} className={`input ${props.className}`}></textarea>
+  );
+}
+
+export function PasswordInput(props: HTMLProps<HTMLInputElement>) {
+  const [visible, setVisible] = useState(false);
+
+  function changeVisibility() {
+    setVisible(!visible);
+  }
+
+  return (
+    <div className="password-input-container">
+      <IconButton
+        icon={visible ? <EyeIcon /> : <EyeOffIcon />}
+        onClick={changeVisibility}
+        className={"password-eye"}
+      />
+      <input
+        {...props}
+        type={visible ? "text" : "password"}
+        className={"password-input"}
+      />
+    </div>
+  );
+}
+
+export interface InputRangeProps {
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  title?: string;
+  value: number | string;
+  className?: string;
+  min: string;
+  max: string;
+  step: string;
+}
+
+export function InputRange({
+  onChange,
+  title,
+  value,
+  className,
+  min,
+  max,
+  step,
+}: InputRangeProps) {
+  return (
+    <div
+      className={`border-light rounded-lg py-2.5 px-3 flex justify-between max-w-[50%] ${
+        className ?? ""
+      }`}
+    >
+      <div className="w-10 text-center">{title || value}</div>
+      <input
+        className="max-w-[calc(100%-40px)]"
+        type="range"
+        title={title}
+        value={value}
+        min={min}
+        max={max}
+        step={step}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
+}
---
+++ src/ui/Loading.tsx
@@ -0,0 +1,9 @@
+import LoadingIcon from "src/icons/three-dots.svg";
+
+export function Loading() {
+  return (
+    <div className="h-full w-full c no-dark">
+      <LoadingIcon />
+    </div>
+  );
+}
