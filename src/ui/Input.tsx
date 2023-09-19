636fe6d7780b3b3c26bbfb02ae3ca49eda0c7a1b
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-19 22:16:48 +0530

    update: ui Input add ExpandingTextArea

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -77,3 +77,22 @@ export function InputRange({
     </div>
   );
 }
+
+export interface TextAreaProps extends React.ComponentPropsWithoutRef<"div"> {}
+
+export const ExpandingTextArea = forwardRef<HTMLDivElement, TextAreaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        contentEditable
+        ref={ref}
+        className={[
+          "whitespace-pre-wrap text-sm resize-none min-h-[40px] before:text-neutral-500 scroll-thin focus:empty:before:content-['Start_typing...'] empty:before:content-['Send_message'] max-h-[320px] bg-box px-3 py-2 rounded overflow-auto w-full focus:outline-none",
+        ].join(" ")}
+        {...props}
+      />
+    );
+  },
+);
+
+ExpandingTextArea.displayName = "TextArea";
