0d8414dfd306f38c431fbd110b0c873d3fa02e2c
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-23 04:39:25 +0530

    feat: add default masks

---
+++ src/masks/index.ts
@@ -0,0 +1,23 @@
+import { Mask } from "../store/mask";
+import { DefaultMasks } from "./default-masks";
+import { type BuiltinMask } from "src/types/Mask";
+
+export const BUILTIN_MASK_ID = 100000;
+
+export const BUILTIN_MASK_STORE = {
+  buildinId: BUILTIN_MASK_ID,
+  masks: {} as Record<string, BuiltinMask>,
+  get(id?: string) {
+    if (!id) return undefined;
+    return this.masks[id] as Mask | undefined;
+  },
+  add(m: BuiltinMask) {
+    const mask = { ...m, id: this.buildinId++, builtin: true };
+    this.masks[mask.id] = mask;
+    return mask;
+  },
+};
+
+export const BUILTIN_MASKS: BuiltinMask[] = [...DefaultMasks].map((m) =>
+  BUILTIN_MASK_STORE.add(m),
+);
