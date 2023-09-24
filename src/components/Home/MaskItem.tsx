813e90e6463c8754e1f76be11fbca1fdccb2d550
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-24 06:14:18 +0530

    feat: add MaskItem in Home component

---
+++ src/components/Home/MaskItem.tsx
@@ -0,0 +1,12 @@
+import { Mask } from "src/store";
+import { MaskAvatar } from "../Mask";
+import styles from "./home.module.scss";
+
+export function MaskItem(props: { mask: Mask; onClick?: () => void }) {
+  return (
+    <div className={styles["mask"]} onClick={props.onClick}>
+      <MaskAvatar mask={props.mask} />
+      <div className={styles["mask-name"] + " one-line"}>{props.mask.name}</div>
+    </div>
+  );
+}
