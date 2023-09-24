36bfe8c4bd2cf0d2b0dc5b0a1f9e56906df0873f
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-24 06:48:52 +0530

    feat: add MessageBox component in ChannelChat

---
+++ src/components/Chat/MessageBox.tsx
@@ -0,0 +1,41 @@
+import { IMessage } from "src/types/IMessage";
+import React from "react";
+import { ImagePreview } from "./ImagePreview";
+
+interface props extends IMessage {
+  dir: "left" | "right";
+}
+
+const dirClassNames = {
+  left: "mr-auto",
+  right: "ml-auto",
+};
+
+export const MessageBox: React.FC<props> = ({
+  content,
+  dir,
+  timestamp,
+  username,
+  delivering,
+  image,
+}) => {
+  return (
+    <div
+      className={[
+        "w-[80%] max-w-3xl p-3 whitespace-pre-wrap bg-box my-2 relative rounded-lg outline-none border-light dark:text-neutral-50",
+        dirClassNames[dir],
+      ].join(" ")}
+    >
+      {image && <ImagePreview src={image} />}
+      <p className="overflow-x-hidden text-sm hover:break-words text-ellipsis">
+        {content}
+      </p>
+      <div className="mt-3 -mb-1 text-xs flex">
+        <div className="flex-grow font-medium">
+          {dir === "left" ? username : delivering ? "✔" : "✔✔"}
+        </div>
+        {new Date(timestamp).toLocaleString()}
+      </div>
+    </div>
+  );
+};
