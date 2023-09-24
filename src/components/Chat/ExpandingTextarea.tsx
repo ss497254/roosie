5290d49664e4207312e694d59893f901488cc011
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-24 06:49:30 +0530

    feat: add ExpandingTextarea component in ChannelChat

---
+++ src/components/Chat/ExpandingTextarea.tsx
@@ -0,0 +1,19 @@
+import { forwardRef } from "react";
+import { TextAreaProps } from "src/ui";
+
+export const ExpandingTextArea = forwardRef<HTMLDivElement, TextAreaProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <div
+        contentEditable
+        ref={ref}
+        className={[
+          "whitespace-pre-wrap text-sm focus:ring-1 focus:ring-primary resize-none min-h-[40px] before:text-neutral-500 scroll-thin focus:empty:before:content-['Start_typing...'] empty:before:content-['Send_message'] max-h-[320px] bg-box px-3 py-2 rounded overflow-auto w-full focus:outline-none",
+        ].join(" ")}
+        {...props}
+      />
+    );
+  },
+);
+
+ExpandingTextArea.displayName = "TextArea";
