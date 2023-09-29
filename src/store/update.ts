dcceaf8284ae872d6ba617331ba05a059ca4aa91
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-30 00:23:03 +0530

    update: remove persist from update store

--- src/store/update.ts
+++ src/store/update.ts
@@ -1,7 +1,5 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
 import { getApiClient } from "src/lib/api-client-store";
-import { StoreKey } from "../constant";
+import { create } from "zustand";
 
 export interface UpdateStore {
   used: number;
@@ -13,39 +11,31 @@ export interface UpdateStore {
 
 const ONE_MINUTE = 60 * 1000;
 
-export const useUpdateStore = create<UpdateStore>()(
-  persist(
-    (set, get) => ({
-      used: 0,
-      subscription: 0,
-      lastUpdateUsage: 0,
+export const useUpdateStore = create<UpdateStore>()((set, get) => ({
+  used: 0,
+  subscription: 0,
+  lastUpdateUsage: 0,
 
-      async updateUsage(force = false) {
-        const overOneMinute = Date.now() - get().lastUpdateUsage >= ONE_MINUTE;
-        if (!overOneMinute && !force) return;
+  async updateUsage(force = false) {
+    const overOneMinute = Date.now() - get().lastUpdateUsage >= ONE_MINUTE;
+    if (!overOneMinute && !force) return;
 
+    set(() => ({
+      lastUpdateUsage: Date.now(),
+    }));
+
+    try {
+      const api = getApiClient();
+      const usage = await api.llm.usage();
+
+      if (usage) {
         set(() => ({
-          lastUpdateUsage: Date.now(),
+          used: usage.used,
+          subscription: usage.total,
         }));
-
-        try {
-          const api = getApiClient();
-          const usage = await api.llm.usage();
-
-          if (usage) {
-            set(() => ({
-              used: usage.used,
-              subscription: usage.total,
-            }));
-          }
-        } catch (e) {
-          console.error((e as Error).message);
-        }
-      },
-    }),
-    {
-      name: StoreKey.Update,
-      version: 1,
-    },
-  ),
-);
+      }
+    } catch (e) {
+      console.error((e as Error).message);
+    }
+  },
+}));
