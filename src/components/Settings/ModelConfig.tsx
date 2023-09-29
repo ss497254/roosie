9da40b3d5e09bebe6a0df621727837a5f3dd2f11
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-30 02:01:30 +0530

    update: MaskConfig in Settings

--- src/components/Settings/ModelConfig.tsx
+++ src/components/Settings/ModelConfig.tsx
@@ -1,31 +1,33 @@
-import { ModalConfigValidator, ModelConfig, useAppConfig } from "src/store";
-import { InputRange, ListItem, Select } from "src/ui";
+import { ModalConfigValidator, useAppConfig } from "src/store";
+import { InputRange, List, ListItem, Select } from "src/ui";
 
-export function ModelConfigList(props: {
-  modelConfig: ModelConfig;
-  updateConfig: (updater: (config: ModelConfig) => void) => void;
+export function ModelConfigList({
+  updateConfig = useAppConfig.getState().update,
 }) {
-  const config = useAppConfig();
+  const modelConfig = useAppConfig((state) => state.modelConfig);
 
   return (
-    <>
+    <List>
       <ListItem title="Model">
         <Select
-          value={props.modelConfig.model}
+          value={modelConfig.model}
           onChange={(e) => {
-            props.updateConfig(
-              (config) =>
-                (config.model = ModalConfigValidator.model(
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.model = ModalConfigValidator.model(
                   e.currentTarget.value,
                 )),
             );
           }}
         >
-          {config.allModels().map((v, i) => (
-            <option value={v.name} key={i} disabled={!v.available}>
-              {v.name}
-            </option>
-          ))}
+          {useAppConfig
+            .getState()
+            .allModels()
+            .map((v, i) => (
+              <option value={v.name} key={i} disabled={!v.available}>
+                {v.name}
+              </option>
+            ))}
         </Select>
       </ListItem>
       <ListItem
@@ -33,14 +35,14 @@ export function ModelConfigList(props: {
         subTitle="A larger value makes the more random output"
       >
         <InputRange
-          value={props.modelConfig.temperature?.toFixed(1)}
+          value={modelConfig.temperature?.toFixed(1)}
           min="0"
           max="1" // lets limit it to 0-1
           step="0.1"
           onChange={(e) => {
-            props.updateConfig(
-              (config) =>
-                (config.temperature = ModalConfigValidator.temperature(
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.temperature = ModalConfigValidator.temperature(
                   e.currentTarget.valueAsNumber,
                 )),
             );
@@ -52,14 +54,14 @@ export function ModelConfigList(props: {
         subTitle="Do not alter this value together with temperature"
       >
         <InputRange
-          value={(props.modelConfig.top_p ?? 1).toFixed(1)}
+          value={(modelConfig.top_p ?? 1).toFixed(1)}
           min="0"
           max="1"
           step="0.1"
           onChange={(e) => {
-            props.updateConfig(
-              (config) =>
-                (config.top_p = ModalConfigValidator.top_p(
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.top_p = ModalConfigValidator.top_p(
                   e.currentTarget.valueAsNumber,
                 )),
             );
@@ -74,11 +76,11 @@ export function ModelConfigList(props: {
           type="number"
           min={100}
           max={100000}
-          value={props.modelConfig.max_tokens}
+          value={modelConfig.max_tokens}
           onChange={(e) =>
-            props.updateConfig(
-              (config) =>
-                (config.max_tokens = ModalConfigValidator.max_tokens(
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.max_tokens = ModalConfigValidator.max_tokens(
                   e.currentTarget.valueAsNumber,
                 )),
             )
@@ -90,14 +92,14 @@ export function ModelConfigList(props: {
         subTitle="A larger value increases the likelihood to talk about new topics"
       >
         <InputRange
-          value={props.modelConfig.presence_penalty?.toFixed(1)}
+          value={modelConfig.presence_penalty?.toFixed(1)}
           min="-2"
           max="2"
           step="0.1"
           onChange={(e) => {
-            props.updateConfig(
-              (config) =>
-                (config.presence_penalty =
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.presence_penalty =
                   ModalConfigValidator.presence_penalty(
                     e.currentTarget.valueAsNumber,
                   )),
@@ -111,14 +113,14 @@ export function ModelConfigList(props: {
         subTitle="A larger value decreasing the likelihood to repeat the same line"
       >
         <InputRange
-          value={props.modelConfig.frequency_penalty?.toFixed(1)}
+          value={modelConfig.frequency_penalty?.toFixed(1)}
           min="-2"
           max="2"
           step="0.1"
           onChange={(e) => {
-            props.updateConfig(
-              (config) =>
-                (config.frequency_penalty =
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.frequency_penalty =
                   ModalConfigValidator.frequency_penalty(
                     e.currentTarget.valueAsNumber,
                   )),
@@ -133,11 +135,12 @@ export function ModelConfigList(props: {
       >
         <input
           type="checkbox"
-          checked={props.modelConfig.enableInjectSystemPrompts}
+          checked={modelConfig.enableInjectSystemPrompts}
           onChange={(e) =>
-            props.updateConfig(
-              (config) =>
-                (config.enableInjectSystemPrompts = e.currentTarget.checked),
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.enableInjectSystemPrompts =
+                  e.currentTarget.checked),
             )
           }
         ></input>
@@ -149,10 +152,11 @@ export function ModelConfigList(props: {
       >
         <input
           type="text"
-          value={props.modelConfig.template}
+          value={modelConfig.template}
           onChange={(e) =>
-            props.updateConfig(
-              (config) => (config.template = e.currentTarget.value),
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.template = e.currentTarget.value),
             )
           }
         ></input>
@@ -163,14 +167,15 @@ export function ModelConfigList(props: {
         subTitle="Number of sent messages attached per request"
       >
         <InputRange
-          title={props.modelConfig.historyMessageCount.toString()}
-          value={props.modelConfig.historyMessageCount}
+          title={modelConfig.historyMessageCount.toString()}
+          value={modelConfig.historyMessageCount}
           min="0"
           max="64"
           step="1"
           onChange={(e) =>
-            props.updateConfig(
-              (config) => (config.historyMessageCount = e.target.valueAsNumber),
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.historyMessageCount = e.target.valueAsNumber),
             )
           }
         ></InputRange>
@@ -184,11 +189,11 @@ export function ModelConfigList(props: {
           type="number"
           min={500}
           max={4000}
-          value={props.modelConfig.compressMessageLengthThreshold}
+          value={modelConfig.compressMessageLengthThreshold}
           onChange={(e) =>
-            props.updateConfig(
-              (config) =>
-                (config.compressMessageLengthThreshold =
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.compressMessageLengthThreshold =
                   e.currentTarget.valueAsNumber),
             )
           }
@@ -197,14 +202,15 @@ export function ModelConfigList(props: {
       <ListItem title="Memory Prompt" subTitle="Send Memory">
         <input
           type="checkbox"
-          checked={props.modelConfig.sendMemory}
+          checked={modelConfig.sendMemory}
           onChange={(e) =>
-            props.updateConfig(
-              (config) => (config.sendMemory = e.currentTarget.checked),
+            updateConfig(
+              ({ modelConfig }) =>
+                (modelConfig.sendMemory = e.currentTarget.checked),
             )
           }
         ></input>
       </ListItem>
-    </>
+    </List>
   );
 }
