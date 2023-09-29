0de163b5fc36ab73725447e11fe0e7b615720cf3
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-30 00:16:43 +0530

    update: add Delete Btn in ChatItem

--- src/components/ChatList/ChatItem.tsx
+++ src/components/ChatList/ChatItem.tsx
@@ -1,5 +1,6 @@
 import { Mask } from "src/store";
-import { MaskAvatar } from "../Mask";
+import DeleteIcon from "src/icons/delete.svg";
+import { IconButton } from "src/ui/IconButtonWithText";
 
 export function ChatItem(props: {
   onClick?: () => void;
@@ -14,26 +15,28 @@ export function ChatItem(props: {
   return (
     <div
       title={`${props.title}\n${props.count} messages`}
-      className={`chat-item ${props.selected && "chat-item-selected"}`}
+      className={[
+        "chat-item group relative",
+        props.selected && "chat-item-selected",
+      ].join(" ")}
       onClick={props.onClick}
     >
-      {props.narrow ? (
-        <div className="chat-item-narrow">
-          <div className="chat-item-avatar no-dark">
-            {props.mask ? <MaskAvatar mask={props.mask} /> : props.title[0]}
-          </div>
-          <div className="chat-item-narrow-count">{props.count}</div>
-        </div>
-      ) : (
-        <>
-          <div className="chat-item-title">{props.title}</div>
-          <div className="chat-item-info">
-            {props.count && (
-              <div className="chat-item-detail">{props.count} messages</div>
-            )}
-            <div className="chat-item-date">{props.time}</div>
-          </div>
-        </>
+      <div className="chat-item-title">{props.title}</div>
+      <div className="chat-item-info">
+        {props.count && (
+          <div className="chat-item-detail">{props.count} messages</div>
+        )}
+        <div className="chat-item-date">{props.time}</div>
+      </div>
+      {props.onDelete && (
+        <IconButton
+          className="absolute right-2 top-2 !hidden group-hover:!flex !p-2"
+          icon={<DeleteIcon />}
+          onClick={(e) => {
+            e.stopPropagation();
+            props.onDelete?.();
+          }}
+        />
       )}
     </div>
   );
