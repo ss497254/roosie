72f06bb52a3453f0dd38fc21c29e1d540b821ed9
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-30 00:18:38 +0530

    update: load channels from access store in ChannelChatList

--- src/components/ChatList/ChannelChatList.tsx
+++ src/components/ChatList/ChannelChatList.tsx
@@ -1,31 +1,14 @@
 import Router, { useRouter } from "next/router";
-import { IChannel } from "src/types/IChannel";
-import { ResponseType } from "src/types/ResposeType";
-import { Spinner } from "src/ui/Spinner";
-import useSWRImmutable from "swr/immutable";
+import { useAccessStore } from "src/store";
 import { ChatItem } from "./ChatItem";
 
 export function ChannelChatList(props: { narrow?: boolean }) {
-  const {
-    isLoading,
-    data: response,
-    error,
-  } = useSWRImmutable<ResponseType<IChannel[]>>("/chats/channels");
   const router = useRouter();
-
-  if (isLoading || error)
-    return (
-      <div className="h-full c">
-        {isLoading ? <Spinner size={24} /> : error.toString()}
-      </div>
-    );
-
-  if (!response || !response.success || !Array.isArray(response.data))
-    return <div>There is some error in loading channels</div>;
+  const channels = useAccessStore((state) => state.channels!);
 
   return (
     <div className="chat-list animate-slide-in-top">
-      {response.data.map((item, idx) => (
+      {channels.map((item, idx) => (
         <ChatItem
           key={idx}
           title={item.name}
