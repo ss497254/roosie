0411aceb703f70bfc8794548131d2f784d5a9827
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-09-30 04:07:46 +0530

    update: loadPrompts on start

--- src/hooks/useLoadData.ts
+++ src/hooks/useLoadData.ts
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
 import { getApiClient } from "src/lib/api-client-store";
-import { useAppConfig } from "src/store";
+import { useAppConfig, usePromptStore } from "src/store";
 
 export function useLoadData() {
-  const config = useAppConfig();
-
   useEffect(() => {
     (async () => {
       const api = getApiClient();
       const models = await api.llm.models();
-      config.mergeModels(models);
+      useAppConfig.getState().mergeModels(models);
     })().catch(console.warn);
+
+    usePromptStore.getState().loadPrompts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 }
