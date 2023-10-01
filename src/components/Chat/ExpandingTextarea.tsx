404a1926a972361c12d7bfbb14e6fad88dbc29c8
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-01 06:19:51 +0530

    fix: ExpandingTextarea font-size

--- src/components/Chat/ExpandingTextarea.tsx
+++ src/components/Chat/ExpandingTextarea.tsx
@@ -8,7 +8,7 @@ export const ExpandingTextArea = forwardRef<HTMLDivElement, TextAreaProps>(
         contentEditable
         ref={ref}
         className={[
-          "whitespace-pre-wrap text-sm focus:ring-1 focus:ring-primary resize-none min-h-[40px] before:text-neutral-500 scroll-thin focus:empty:before:content-['Start_typing...'] empty:before:content-['Send_message'] max-h-[320px] bg-box px-3 py-2 rounded overflow-auto w-full focus:outline-none",
+          "whitespace-pre-wrap text-base focus:ring-1 focus:ring-primary resize-none min-h-[40px] before:text-neutral-500 scroll-thin focus:empty:before:content-['Start_typing...'] empty:before:content-['Send_message'] max-h-[320px] bg-box px-3 py-2 rounded overflow-auto w-full focus:outline-none cursor-text",
         ].join(" ")}
         {...props}
       />
