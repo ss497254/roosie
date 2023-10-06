960031ca050a7c69b66a91f1210dd479cf6759a5
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-10-06 17:02:08 +0530

    update: load user details on mount

--- src/hooks/useLoadData.ts
+++ src/hooks/useLoadData.ts
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
 import { getApiClient } from "src/lib/api-client-store";
-import { useAppConfig, usePromptStore } from "src/store";
+import { useAccessStore, useAppConfig, usePromptStore } from "src/store";
+import { Cfetch } from "src/utils/fetch";
+
+async function LoadModels() {
+  const api = getApiClient();
+
+  const models = await api.llm.models();
+  useAppConfig.getState().mergeModels(models);
+}
+
+async function LoadUserDetails() {
+  const res = await Cfetch<any>("/user/me");
+
+  if (res && res.success) useAccessStore.setState({ ...res.data });
+}
 
 export function useLoadData() {
   useEffect(() => {
-    (async () => {
-      const api = getApiClient();
-      const models = await api.llm.models();
-      useAppConfig.getState().mergeModels(models);
-    })().catch(console.warn);
+    Promise.all([LoadModels(), LoadUserDetails()]).catch(console.warn);
 
     usePromptStore.getState().loadPrompts();
     // eslint-disable-next-line react-hooks/exhaustive-deps
