165998dc21900ee0f8807a0115479bb9cda6f91e
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-12-04 00:07:19 +0530

    udpate: useTheme hook

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -6,12 +6,18 @@ export function useTheme() {
   const theme = useAppConfig((state) => state.theme);
 
   useEffect(() => {
+    const { primaryColor, secondaryColor } = useAppConfig.getState();
+
     document.body.classList.remove("light");
     document.body.classList.remove("dark");
 
     if (theme === "dark") {
+      document.body.style.setProperty("--primary", "#10b981");
+      document.body.style.setProperty("--secondary", "#e0f2fe");
       document.body.classList.add("dark");
     } else if (theme === "light") {
+      document.body.style.setProperty("--primary", primaryColor);
+      document.body.style.setProperty("--secondary", secondaryColor);
       document.body.classList.add("light");
     }
 
