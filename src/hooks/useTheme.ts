8fe536c3bfb84fd5761db6903118b33f849d7ec5
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-12-04 00:55:44 +0530

    update: fix dark mode colors

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -6,16 +6,16 @@ export function useTheme() {
   const theme = useAppConfig((state) => state.theme);
 
   useEffect(() => {
-    const { primaryColor, secondaryColor } = useAppConfig.getState();
-
     document.body.classList.remove("light");
     document.body.classList.remove("dark");
 
     if (theme === "dark") {
-      document.body.style.setProperty("--primary", "#10b981");
-      document.body.style.setProperty("--secondary", "#e0f2fe");
+      document.body.style.setProperty("--primary", "#059669");
+      document.body.style.setProperty("--secondary", "#1b262a");
       document.body.classList.add("dark");
     } else if (theme === "light") {
+      const { primaryColor, secondaryColor } = useAppConfig.getState();
+
       document.body.style.setProperty("--primary", primaryColor);
       document.body.style.setProperty("--secondary", secondaryColor);
       document.body.classList.add("light");
