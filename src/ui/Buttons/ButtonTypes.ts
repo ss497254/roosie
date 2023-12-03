b4254e164bb7c82d566876d8c177359ea5ccf2ce
Saurabh Singh <73827307+ss497254@users.noreply.github.com>
2023-12-04 00:26:41 +0530

    update: default ButtonType

--- src/ui/Buttons/ButtonTypes.ts
+++ src/ui/Buttons/ButtonTypes.ts
@@ -1,5 +1,5 @@
 export const ButtonTypes = {
-  default: "bg-primary hover:bg-blue-500 text-zinc-50",
+  default: "bg-primary hover:scale-105 text-zinc-50",
   info: "bg-blue-500 hover:bg-blue-600 text-zinc-50",
   accent: "bg-indigo-600 hover:bg-indigo-500 text-zinc-50",
   success: "bg-emerald-600 hover:bg-emerald-500 text-zinc-50",
